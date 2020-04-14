c79cc58e30929c478fc5683ec0129dfd1b19bd53
sanial <nicolas.sanial@protonmail.com>
2020-04-14 22:12:52 +0200

    basic security implemented, mayde implement Oauth2 if i got time

--- src/main/java/sanial/netheos/demoapi/DemoapiApplication.java
+++ src/main/java/sanial/netheos/demoapi/DemoapiApplication.java
@@ -1,7 +1,15 @@
 package sanial.netheos.demoapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sanial.netheos.demoapi.core.model.Faq;
+import sanial.netheos.demoapi.core.model.FaqTag;
+import sanial.netheos.demoapi.core.model.Tag;
+import sanial.netheos.demoapi.core.repository.FaqRepository;
+import sanial.netheos.demoapi.core.repository.FaqTagRepository;
+import sanial.netheos.demoapi.core.repository.TagRepository;
 
 @SpringBootApplication
 public class DemoapiApplication {
@@ -10,4 +18,14 @@ public class DemoapiApplication {
 		SpringApplication.run(DemoapiApplication.class, args);
 	}
 
+	//uncomment this following lines to auto generate data in database
+	/*
+	@Bean
+	CommandLineRunner initDatabase(FaqRepository faqRepository, TagRepository tagRepository, FaqTagRepository faqTagRepository) {
+		return args -> {
+			faqTagRepository.save(new FaqTag(new Faq("Pouvez vous garantir la robustesse du produit ?", "Nous sommes certains d'utiliser des technos à jour et nous respectons les normes de code"), new Tag("Sécurité")));
+			faqTagRepository.save(new FaqTag(new Faq("Comment sont gérés les informations utilisateurs au sein de votre produit ?", "Notre produit est RGPD compliant, nous respectons lse normes imposées par la CNIL"), new Tag("Robustesse")));
+		};
+	}
+	*/
 }
--- src/main/java/sanial/netheos/demoapi/api/config/ApiSecurityConfig.java
+++ src/main/java/sanial/netheos/demoapi/api/config/ApiSecurityConfig.java
@@ -1,20 +1,45 @@
 package sanial.netheos.demoapi.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String ROLE_USER = "USER";
+    private final String ROLE_ADMIN = "ADMIN";
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(env.getProperty("api.user.login")).password(encoder.encode(env.getProperty("api.user.password"))).roles(ROLE_USER)
+                .and()
+                .withUser(env.getProperty("api.admin.login")).password(encoder.encode(env.getProperty("api.admin.password"))).roles(ROLE_USER, ROLE_ADMIN);
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.POST,"/api/faqtag/create").permitAll()
-                .antMatchers(HttpMethod.POST,"/api/faq/search").permitAll()
-                .antMatchers(HttpMethod.GET,"/api/faq").permitAll()
-                .anyRequest().authenticated();
+        BasicAuthenticationEntryPoint entryPoint = new BasicAuthenticationEntryPoint();
+        entryPoint.setRealmName("Demo API Netheos");
+
+        http.csrf().disable().antMatcher("/api/**").authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/faqtag/create").hasRole(ROLE_ADMIN)
+                .antMatchers(HttpMethod.POST,"/api/faq/search").hasRole(ROLE_USER)
+                .antMatchers(HttpMethod.GET,"/api/faq").hasRole(ROLE_ADMIN)
+                .and().httpBasic().authenticationEntryPoint(entryPoint);
     }
 }
 
